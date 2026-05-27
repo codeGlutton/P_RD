9cd31aaceff5a86d8ad3cf6b80872a1afdb4047b
Mo <98795422+codeGlutton@users.noreply.github.com>
2026-05-28 05:55:52 +0900

    style Unit Team Type 열거형 항목 이름 수정

--- Source/P_RD/Setting/UnitTeamType.h
+++ Source/P_RD/Setting/UnitTeamType.h
@@ -22,7 +22,7 @@ namespace EUnitTeamType
         AllNeutral = 0      UMETA(ToolTip = "중립 팀"),
         AllHostile          UMETA(ToolTip = "자신 포함 모두 적대하는 팀"),
         Adventurer          UMETA(ToolTip = "플레이어 팀"),
-        Monster             UMETA(ToolTip = "몬스터 팀"),
+        Enemy               UMETA(ToolTip = "몬스터 팀"),
         NPC                 UMETA(ToolTip = "NPC 팀"),
         Count               UMETA(Hidden)
     };
