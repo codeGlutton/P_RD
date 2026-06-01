d4e80d3da3933ebc19901af2b5c5da769293c6e8
Kim JunHyeong <juny000903@gmail.com>
2026-06-01 18:11:45 +0900

    feat GA_Base 생성

---
+++ Source/P_RD/GAS/Ability/GameplayAbility_Base.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "../GASMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GameplayAbility_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P_RD_API UGameplayAbility_Base : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UGameplayAbility_Base();
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ability", meta = (AllowPrivateAccess = "true"))
+	float		mDiceCost;
+
+	TSubclassOf<UGameplayEffect>	mDiceCostClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Ability", meta = (AllowPrivateAccess = "true"))
+	float		mDiceDots;
+
+	TSubclassOf<UGameplayEffect>	mDiceDotsClass;
+
+};
