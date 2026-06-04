c75287c85cd0598c1f598732756090b8ad88d49f
Kim JunHyeong <juny000903@gmail.com>
2026-06-04 13:37:47 +0900

    fix EEffectFilter -> ETargetScope, ETargetFilter로 분리
    ETargetScope : 효과를 어떤 대상에게 줄 것인가?
    ETargetFilter : 어떤 대상을 제외할 것인가?

--- Source/P_RD/DataAsset/SkillData/StaticSkillData.h
+++ Source/P_RD/DataAsset/SkillData/StaticSkillData.h
@@ -14,37 +14,44 @@
 #include "SkillType.h"
 #include "StaticSkillData.generated.h"
 
- /**
- * @brief 필터 유형
- * @details
- * 추후 위치 변경 예정
- */
-UENUM(BlueprintType)
-enum class EEffectFilter : uint8
-{
-    Caster = 0,
-    Others,
-    All,
-    Count		UMETA(Hidden)
-};
 
+ /**
+ * @brief 효과 발동 구조체
+ *
+ * @details
+ * 효과 발동 시 타겟, 수치 등을 모두 개별적으로 관리할 수 있도록 구조체로 구분지었다.
+ */
 USTRUCT(BlueprintType)
 struct FGameplayEffectData
 {
     GENERATED_BODY()
 
     /**
-    * @brief 필터 유형
+    * @brief 선택 대상
+    * 
     * @details
-    * (Caster: 시전자만, All: 영향 범위 전체, Others: 시전자 제외 모든 타일)
+    * (Caster: 시전자만, Target: 선택된 대상, Both: 시전자 + 선택된 대상)
     */
-    UPROPERTY(Category = "GameplayEffectData", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "EffectTarget"))
-    EEffectFilter mGameplayEffectFilter;
+    UPROPERTY(Category = "GameplayEffectData", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "TargetScope"))
+    ETargetScope mTargetScope;
+
+    /**
+    * @brief 제외 대상
+    * 
+    * @details
+    * 없음, 자신, 아군, 적
+    */
+    UPROPERTY(Category = "GameplayEffectData", EditAnywhere, BlueprintReadWrite, meta = (Bitmask, BitmaskEnum = "/Script/P_RD.ETargetFilter", DisplayName = "TargetFilter"))
+    uint8 mTargetFilter = 3;
 
     /**
     * @brief 효과
+    * 
     * @details
 	* GameplayEffect_Base를 상속받은 Blueprint Class를 참조하는 SoftClassPtr
+    * 
+    * @note
+    * Damage, Heal 기타 등등
     */
     UPROPERTY(Category = "GameplayEffectData", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "GameplayEffect"))
     TSoftClassPtr<class UGameplayEffect_Base> mGameplayEffect;
@@ -66,6 +73,12 @@ struct FGameplayEffectData
     float mGameplayEffectRatioValue;
 };
 
+/**
+* @brief 액션과 여러 효과들을 묶은 구조체
+* 
+* @details
+* 하나의 애니메이션 발동 시 여러 효과가 발동 할 수 있게 구조체로 묶었다.
+*/
 USTRUCT(BlueprintType)
 struct FSkillAction
 {
@@ -73,16 +86,21 @@ struct FSkillAction
 
     /**
     * @brief 보여줄 애니메이션
+    * 
     * @details
     * (ex: Anim.Attack, Anim.Spell)
+    * 
+    * @note
+    * 애니메이션은 아직 확정된 사항이 없으므로 추후 변경될 것이다.
     */
     UPROPERTY(Category = "SkillAction", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "Animation"))
     FGameplayTag mAnimationTag;
 
     /**
     * @brief 효과 레이어
-    * @details
     * 
+    * @details
+    * notify 호출 시 모든 효과들이 발동될 것이다.
     */
     UPROPERTY(Category = "SkillAction", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "GameplayEffectDatas"))
     TArray<FGameplayEffectData> mGameplayEffectDatas;
@@ -117,14 +135,19 @@ public:
 
     /**
     * @brief 스킬 유형
+    * 
     * @details
     * 공격, 주문
+    * 
+    * @note
+    * Spell과 Attack 스킬이 자식 클래스로 분리되어 있으므로 생성자에서 설정하도록 const로 바꿔도 좋을 것 같다.
     */
     UPROPERTY(Category = "Default", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "SkillType"))
     ESkillType mSkillType;
     
     /**
     * @brief 스킬 희귀도
+    * 
     * @details
     * Primary Asset을 스킬 타입과 희귀도로 분류해두었기 때문에, 해당 값은 Primary Asset Type에 영향을 줌
     */
@@ -146,13 +169,13 @@ public:
 
     /**
     * @brief 조준 범위 유형
-    * @details
     */
     UPROPERTY(Category = "AimRange", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "AimType"))
     EAimPattern mAimPattern;
 
     /**
     * @brief 곡사 여부
+    * 
     * @details 
     * true면 적 넘어도 선택 가능
     * false면 적 넘어는 선택 불가
@@ -162,6 +185,7 @@ public:
 
     /**
     * @brief 장애물(유닛) 조준 가능 여부
+    * 
     * @details 
     * true면 장애물(유닛)이 있는 타일 선택 가능
     * false면 장애물(유닛)이 있는 타일 선택 불가
@@ -171,17 +195,20 @@ public:
 
     /**
     * @brief 기본 조준 거리
+    * 
     * @details 
     * 고정값
+    * 사정거리는 항상 DefaultRange + RatioRange * 눈금
     */
     UPROPERTY(Category = "AimRange", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "AimDefaultRange"))
     int32 mAimDefaultRange;
 
     /**
     * @brief 비율 조준 거리
+    * 
     * @details
     * 주사위값에 따라 변하는 조준 범위 
-    * 사정거리는 항상 DefaultRange + RatioRange
+    * 사정거리는 항상 DefaultRange + RatioRange * 눈금
     */
     UPROPERTY(Category = "AimRange", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "AimRatioRange"))
     float mAimRatioRange;
@@ -194,6 +221,7 @@ public:
 
     /**
     * @brief 영향 범위 유형
+    * 
     * @details
     */
     UPROPERTY(Category = "EffectArea", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "EffectAreaType"))
@@ -201,6 +229,7 @@ public:
 
     /**
     * @brief 관통 여부
+    * 
     * @details
     * true면 유닛 넘어도 영향 대상
     * false면 유닛 넘어는 영향 대상이 아님
@@ -210,17 +239,20 @@ public:
 
     /**
     * @brief 기본 영향 범위
+    * 
     * @details
     * 고정값
+    * 사정거리는 항상 DefaultArea + RatioArea * 눈금
     */
     UPROPERTY(Category = "EffectArea", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "EffectDefaultArea"))
     int32 mEffectDefaultArea;
 
     /**
     * @brief 비율 영향 범위
+    * 
     * @details
     * 주사위값에 따라 변하는 영향 범위 비율
-    * 사정거리는 항상 DefaultArea + RatioArea
+    * 사정거리는 항상 DefaultArea + RatioArea * 눈금
     */
     UPROPERTY(Category = "EffectArea", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "EffectRatioArea"))
     float mEffectRatioArea;
@@ -228,9 +260,10 @@ public:
 #pragma endregion EffectArea
 
     /**
-    * @brief 스킬 효과
-    * @details
+    * @brief 액션 
     * 
+    * @details
+    * 하나의 애니메이션 발동 시 여러 효과가 발동 할 수 있게 구조체로 묶었다.
     */
     UPROPERTY(Category = "SkillAction", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "SkillActions"))
     TArray<FSkillAction> mSkillActions;
