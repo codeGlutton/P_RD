1d74733dab39bc7266c9a4d2bfe18b5e00f35cda
2009ejsvk <2009ejsvk@gmail.com>
2026-06-09 15:55:12 +0900

    Use toggleable widget base class

--- Source/P_RD/UI/CinematicWidget.cpp
+++ Source/P_RD/UI/CinematicWidget.cpp
@@ -3,118 +3,34 @@
 UCinematicWidget::UCinematicWidget(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
-	SetVisibility(ESlateVisibility::Collapsed);
-}
-
-void UCinematicWidget::OpenUI(FOnEndUIOpenAnimation Callback)
-{
-	OnEndUIOpenAnimation = MoveTemp(Callback);
-	bOpenUIFinished = false;
-	LifecycleState = ECinematicWidgetLifecycleState::Opening;
-	if (IsInViewport() == false)
-	{
-		AddToViewport();
-	}
-
-	SetVisibility(ESlateVisibility::Visible);
-	PlayOpenUIAnimation();
-}
-
-void UCinematicWidget::CloseUI(FOnEndUICloseAnimation Callback)
-{
-	if (LifecycleState == ECinematicWidgetLifecycleState::Closed || LifecycleState == ECinematicWidgetLifecycleState::Closing)
-	{
-		if (Callback.IsBound())
-		{
-			Callback.Execute(this);
-		}
-		return;
-	}
-
-	OnEndUICloseAnimation = MoveTemp(Callback);
-	bCloseUIFinished = false;
-	LifecycleState = ECinematicWidgetLifecycleState::Closing;
-	PlayCloseUIAnimation();
-}
-
-bool UCinematicWidget::IsOpened() const
-{
-	return LifecycleState == ECinematicWidgetLifecycleState::Opening
-		|| LifecycleState == ECinematicWidgetLifecycleState::Open
-		|| LifecycleState == ECinematicWidgetLifecycleState::Playing;
+	bRemoveFromParentOnClose = true;
 }
 
 void UCinematicWidget::PlayCinematic(FOnEndCinematicAnimation Callback)
 {
-	if (LifecycleState == ECinematicWidgetLifecycleState::Closed || LifecycleState == ECinematicWidgetLifecycleState::Closing)
+	if (IsOpened() == false)
 	{
 		return;
 	}
 
 	OnEndCinematicAnimation = MoveTemp(Callback);
 	bCinematicFinished = false;
-	LifecycleState = ECinematicWidgetLifecycleState::Playing;
 	PlayCinematicAnimation();
-}
-
-void UCinematicWidget::FinishOpenUI()
-{
-	if (bOpenUIFinished || LifecycleState != ECinematicWidgetLifecycleState::Opening)
-	{
-		return;
-	}
-
-	bOpenUIFinished = true;
-	LifecycleState = ECinematicWidgetLifecycleState::Open;
-	if (OnEndUIOpenAnimation.IsBound())
-	{
-		OnEndUIOpenAnimation.Execute(this);
-		OnEndUIOpenAnimation.Unbind();
-	}
-}
-
-void UCinematicWidget::FinishCloseUI()
-{
-	if (bCloseUIFinished || LifecycleState != ECinematicWidgetLifecycleState::Closing)
-	{
-		return;
-	}
-
-	bCloseUIFinished = true;
-	SetVisibility(ESlateVisibility::Collapsed);
-	RemoveFromParent();
-	LifecycleState = ECinematicWidgetLifecycleState::Closed;
-	if (OnEndUICloseAnimation.IsBound())
-	{
-		OnEndUICloseAnimation.Execute(this);
-		OnEndUICloseAnimation.Unbind();
-	}
 }
 
 void UCinematicWidget::FinishCinematic()
 {
-	if (bCinematicFinished || LifecycleState != ECinematicWidgetLifecycleState::Playing)
+	if (bCinematicFinished)
 	{
 		return;
 	}
 
 	bCinematicFinished = true;
-	LifecycleState = ECinematicWidgetLifecycleState::Open;
 	if (OnEndCinematicAnimation.IsBound())
 	{
 		OnEndCinematicAnimation.Execute(this);
 		OnEndCinematicAnimation.Unbind();
 	}
-}
-
-void UCinematicWidget::PlayOpenUIAnimation_Implementation()
-{
-	FinishOpenUI();
-}
-
-void UCinematicWidget::PlayCloseUIAnimation_Implementation()
-{
-	FinishCloseUI();
 }
 
 void UCinematicWidget::PlayCinematicAnimation_Implementation()
--- Source/P_RD/UI/CinematicWidget.h
+++ Source/P_RD/UI/CinematicWidget.h
@@ -7,8 +7,7 @@
 #pragma once
 
 #include "RDMinimal.h"
-#include "Blueprint/UserWidget.h"
-#include "UI/ToggleableWidgetInterface.h"
+#include "UI/ToggleableWidget.h"
 
 #include "CinematicWidget.generated.h"
 
@@ -20,57 +19,24 @@ DECLARE_DELEGATE_OneParam(FOnEndCinematicAnimation, UCinematicWidget*)
  * @brief 시네마틱 표시, UI 열림/닫힘, 재생 완료 알림을 제공하는 위젯 베이스
  */
 UCLASS(BlueprintType, Blueprintable)
-class P_RD_API UCinematicWidget : public UUserWidget, public IToggleableWidgetInterface
+class P_RD_API UCinematicWidget : public UToggleableWidget
 {
 	GENERATED_BODY()
 
 public:
 	UCinematicWidget(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
 
-	void OpenUI(FOnEndUIOpenAnimation Callback = FOnEndUIOpenAnimation()) override;
-	void CloseUI(FOnEndUICloseAnimation Callback = FOnEndUICloseAnimation()) override;
-	bool IsOpened() const override;
-
 	void PlayCinematic(FOnEndCinematicAnimation Callback = FOnEndCinematicAnimation());
-
-	UFUNCTION(BlueprintCallable, Category = "UI|Cinematic")
-	void FinishOpenUI();
-
-	UFUNCTION(BlueprintCallable, Category = "UI|Cinematic")
-	void FinishCloseUI();
 
 	UFUNCTION(BlueprintCallable, Category = "UI|Cinematic")
 	void FinishCinematic();
 
 protected:
 	UFUNCTION(BlueprintNativeEvent, Category = "UI|Cinematic")
-	void PlayOpenUIAnimation();
-	virtual void PlayOpenUIAnimation_Implementation();
-
-	UFUNCTION(BlueprintNativeEvent, Category = "UI|Cinematic")
-	void PlayCloseUIAnimation();
-	virtual void PlayCloseUIAnimation_Implementation();
-
-	UFUNCTION(BlueprintNativeEvent, Category = "UI|Cinematic")
 	void PlayCinematicAnimation();
 	virtual void PlayCinematicAnimation_Implementation();
 
 private:
-	enum class ECinematicWidgetLifecycleState : uint8
-	{
-		Closed,
-		Opening,
-		Open,
-		Playing,
-		Closing,
-	};
-
-	FOnEndUIOpenAnimation OnEndUIOpenAnimation;
-	FOnEndUICloseAnimation OnEndUICloseAnimation;
 	FOnEndCinematicAnimation OnEndCinematicAnimation;
-
-	ECinematicWidgetLifecycleState LifecycleState = ECinematicWidgetLifecycleState::Closed;
-	bool bOpenUIFinished = false;
-	bool bCloseUIFinished = false;
 	bool bCinematicFinished = false;
 };
