9f5a19710a654253845d9d11c7fc328aafcfb90a
2009ejsvk <2009ejsvk@gmail.com>
2026-06-11 22:13:28 +0900

    fix combat camera view target

--- Source/P_RD/GameMode/CombatGameMode.h
+++ Source/P_RD/GameMode/CombatGameMode.h
@@ -21,4 +21,15 @@ class P_RD_API ACombatGameMode : public ARoomGameModeBase
 protected:
 	void InitializeRoom() override;
 	void BeginRoom() override;
+
+private:
+	/**
+	 * @brief 전투맵 WorldSettings에 지정된 카메라 기준점으로 플레이어 시점을 맞춘다.
+	 *
+	 * @details
+	 * MainCameraPoint는 에디터에서 방마다 배치하는 TargetPoint다.
+	 * 이 값은 카메라 액터 자체가 아니라 "전투를 볼 위치"를 표시하는 기준점이므로,
+	 * 전투방이 시작될 때 같은 위치에 CameraActor를 만들고 RoomStartPoint 방향으로 돌려 ViewTarget으로 지정한다.
+	 */
+	void ApplyMainCameraPoint() const;
 };
