b1856d65edb44a32d3efc4a365741d9ed49eeaa0
2009ejsvk <2009ejsvk@gmail.com>
2026-06-11 22:13:28 +0900

    fix combat camera view target

--- Source/P_RD/GameMode/CombatGameMode.cpp
+++ Source/P_RD/GameMode/CombatGameMode.cpp
@@ -1,9 +1,12 @@
 ﻿#include "GameMode/CombatGameMode.h"
 #include "Singleton/WorldSubsystem/SRPGCombatSubsystem.h"
 
+#include "Camera/CameraActor.h"
 #include "Engine/AssetManager.h"
+#include "GameFramework/PlayerController.h"
 #include "Singleton/InstanceSubsystem/PersistentData.h"
 #include "DataAsset/RoomSpawnData/StaticCombatRoomSpawnData.h"
+#include "Setting/RDWorldSettings.h"
 
 #include "PCGStage/Room.h"
 
@@ -28,10 +31,37 @@ void ACombatGameMode::BeginRoom()
 {
 	Super::BeginRoom();
 
+	ApplyMainCameraPoint();
+
 	USRPGCombatSubsystem* CombatSubsystem = GetWorld()->GetSubsystem<USRPGCombatSubsystem>();
 	for (const TObjectPtr<AUnit>& Unit : CombatSubsystem->GetUnits())
 	{
 		Unit->OnBeginPlayRoom();
 	}
 	CombatSubsystem->BeginCombat();
+}
+
+void ACombatGameMode::ApplyMainCameraPoint() const
+{
+	UWorld* World = GetWorld();
+	checkf(World != nullptr, TEXT("월드 nullptr"));
+
+	const ARDWorldSettings* WorldSettings = Cast<ARDWorldSettings>(World->GetWorldSettings());
+	checkf(WorldSettings != nullptr, TEXT("RDWorldSettings nullptr"));
+
+	AActor* MainCameraPoint = WorldSettings->GetMainCameraPoint();
+	checkf(MainCameraPoint != nullptr, TEXT("MainCameraPoint nullptr"));
+
+	AActor* RoomStartPoint = WorldSettings->GetRoomStartPoint();
+	checkf(RoomStartPoint != nullptr, TEXT("RoomStartPoint nullptr"));
+
+	APlayerController* PlayerController = World->GetFirstPlayerController();
+	checkf(PlayerController != nullptr, TEXT("플레이어 컨트롤러 nullptr"));
+
+	const FRotator CameraRotation = (RoomStartPoint->GetActorLocation() - MainCameraPoint->GetActorLocation()).Rotation();
+	ACameraActor* CombatCamera = World->SpawnActor<ACameraActor>(ACameraActor::StaticClass(), MainCameraPoint->GetActorLocation(), CameraRotation);
+	checkf(CombatCamera != nullptr, TEXT("전투 카메라 nullptr"));
+
+	PlayerController->SetViewTarget(CombatCamera);
+	PlayerController->SetControlRotation(CameraRotation);
 }
