2e530d9c5669a5c5b83bd56c5067d3e4172f69fd
2009ejsvk <2009ejsvk@gmail.com>
2026-06-15 11:41:48 +0900

    refactor(ui): split skill panel widget
    (cherry picked from commit 1f1dc33853093ddb4d66f3691418be92de59b9a4)
    (cherry picked from commit 13b047a2a94ae1638c636ab8389d64178891681d)

--- Source/P_RD/UI/SkillPanelWidget.cpp
+++ Source/P_RD/UI/SkillPanelWidget.cpp
@@ -1,5 +1,8 @@
 #include "UI/SkillPanelWidget.h"
 
+#include "Components/Button.h"
+#include "UI/SkillPanelWidgetPrivate.h"
+
 /**
  * @brief 스킬 패널 기본 객체를 생성한다.
  *
@@ -10,4 +13,56 @@
 USkillPanelWidget::USkillPanelWidget(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
+}
+
+void USkillPanelWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	EnsureSkillSelectButtons();
+	EnsureSkillDetailWidgets();
+}
+
+void USkillPanelWidget::NativeDestruct()
+{
+	UnbindSkillSelectButtons();
+
+	if (mSkillDetailBackButton != nullptr)
+	{
+		mSkillDetailBackButton->OnClicked.RemoveDynamic(this, &USkillPanelWidget::HandleSkillDetailBackButtonClicked);
+	}
+	if (mSkillDetailPreviousButton != nullptr)
+	{
+		mSkillDetailPreviousButton->OnClicked.RemoveDynamic(this, &USkillPanelWidget::HandleSkillDetailPreviousButtonClicked);
+	}
+	if (mSkillDetailNextButton != nullptr)
+	{
+		mSkillDetailNextButton->OnClicked.RemoveDynamic(this, &USkillPanelWidget::HandleSkillDetailNextButtonClicked);
+	}
+
+	Super::NativeDestruct();
+}
+
+void USkillPanelWidget::ApplyOpenUI()
+{
+	mShowingSkillDetail = false;
+	mDetailSkillIndex = INDEX_NONE;
+	mTrackingSkillDetailSwipe = false;
+	mSkillDetailSwipeStartPosition = FVector2D::ZeroVector;
+
+	Super::ApplyOpenUI();
+	EnsureSkillSelectButtons();
+	EnsureSkillDetailWidgets();
+	RefreshSkillDetailWidgets();
+	RefreshSkillSelectButtons();
+}
+
+int32 USkillPanelWidget::GetCarouselItemDisplayCount() const
+{
+	return RDSkillPanel::ItemCount;
+}
+
+void USkillPanelWidget::HandleCarouselSelectionChanged(int32 PreviousIndex, int32 NewIndex)
+{
+	ShowSkillDetail(NewIndex);
 }
