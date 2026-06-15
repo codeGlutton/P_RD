0cdc9d704b612f060faba881c974ba2906b3c0d3
2009ejsvk <2009ejsvk@gmail.com>
2026-06-15 11:41:42 +0900

    refactor(ui): add panel navigation style helper

---
+++ Source/P_RD/UI/PanelNavigationStyle.cpp
@@ -0,0 +1,36 @@
+#include "UI/PanelNavigationStyle.h"
+
+FLinearColor RDPanelNavigationStyle::GetButtonTextColor()
+{
+	return FLinearColor(0.05f, 0.08f, 0.08f, 1.0f);
+}
+
+FLinearColor RDPanelNavigationStyle::GetSelectedButtonTextColor()
+{
+	return FLinearColor(0.05f, 0.05f, 0.04f, 1.0f);
+}
+
+FLinearColor RDPanelNavigationStyle::GetPanelTextColor()
+{
+	return FLinearColor(0.86f, 1.0f, 0.94f, 1.0f);
+}
+
+FLinearColor RDPanelNavigationStyle::GetOptionButtonColor()
+{
+	return FLinearColor(0.80f, 0.96f, 0.90f, 0.86f);
+}
+
+FLinearColor RDPanelNavigationStyle::GetNavigationButtonColor()
+{
+	return FLinearColor(0.80f, 0.96f, 0.90f, 0.94f);
+}
+
+FLinearColor RDPanelNavigationStyle::GetBackButtonColor()
+{
+	return FLinearColor(0.94f, 0.84f, 0.56f, 0.96f);
+}
+
+FLinearColor RDPanelNavigationStyle::GetSelectedButtonColor()
+{
+	return FLinearColor(0.94f, 0.84f, 0.56f, 0.98f);
+}
---
+++ Source/P_RD/UI/PanelNavigationStyle.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "RDMinimal.h"
+
+namespace RDPanelNavigationStyle
+{
+	P_RD_API FLinearColor GetButtonTextColor();
+	P_RD_API FLinearColor GetSelectedButtonTextColor();
+	P_RD_API FLinearColor GetPanelTextColor();
+	P_RD_API FLinearColor GetOptionButtonColor();
+	P_RD_API FLinearColor GetNavigationButtonColor();
+	P_RD_API FLinearColor GetBackButtonColor();
+	P_RD_API FLinearColor GetSelectedButtonColor();
+}
