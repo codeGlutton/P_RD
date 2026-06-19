97d8dc41715f9aac7c623ea020f20d9fcdd21e7e
Kim JunHyeong <juny000903@gmail.com>
2026-06-19 18:48:13 +0900

    feat Get, Set, delegate 추가
    [SVN-REV]: 37

--- Source/P_RD/Component/AttributeComponent/AttributeSetComponentModel.h
+++ Source/P_RD/Component/AttributeComponent/AttributeSetComponentModel.h
@@ -24,6 +24,8 @@ protected:
 	UPROPERTY(Category = GAS, VisibleAnywhere, meta = (DisplayName = "UnitAttributeSet"))
 	TObjectPtr<UUnitAttributeSet> mUnitAttributeSet;
 
+    TMap<FGameplayAttribute, FOnGameplayAttributeValueChange> DelegateMap;
+
 public:
 	virtual void Initialize() override;
 
@@ -31,5 +33,27 @@ public:
 	virtual void BeginPlay() override;
 
 public:
-	UUnitAttributeSet* GetUnitAttributeSet() const;
+	/*
+    * @brief 해당하는 속성의 값을 가져온다.
+    */
+    float GetAttributeValue(FGameplayAttribute Attribute) const;
+
+    /*
+    * @brief 델리게이트를 찾아서 반환합니다.
+    */
+    FOnGameplayAttributeValueChange& GetGameplayAttributeValueChangeDelegate(FGameplayAttribute Attribute)
+    {
+        return DelegateMap.FindOrAdd(Attribute);
+    }
+    /*
+    * @brief 속성값을 변경합니다.
+    * 
+    * @details
+    * 변경하면서 델리게이트를 작동합니다.
+    * 어트리뷰트셋 수정에 제한을 두고 싶어 우선은 사용 자제 부탁드립니다.
+    * 
+    * @note
+    * 속도가 느리다고 함. 추후 변경 요망
+    */
+    void SetAttributeValue(FGameplayAttribute Attribute, float NewValue);
 };
