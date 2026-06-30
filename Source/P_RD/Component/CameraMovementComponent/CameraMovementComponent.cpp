7846a207d869f66f569f7224b61f4dd407309d56
Kim JunHyeong <juny000903@gmail.com>
2026-06-30 11:33:58 +0900

    feat CombatCameraPawn, CameraMovementComponent
    [SVN-REV]: 114M

---
+++ Source/P_RD/Component/CameraMovementComponent/CameraMovementComponent.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Component/CameraMovementComponent/CameraMovementComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values for this component's properties
+UCameraMovementComponent::UCameraMovementComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UCameraMovementComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UCameraMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UCameraMovementComponent::SetCameraComponent(UCameraComponent* CameraComponent)
+{
+	mCameraComponent = CameraComponent;
+}
+
+UCameraComponent* UCameraMovementComponent::GetCameraComponent()
+{
+	return mCameraComponent.Get();
+}
---
+++ Source/P_RD/Pawn/Camera/CombatCameraPawn.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawn/Camera/CombatCameraPawn.h"
+#include "Camera/CameraComponent.h"
+#include "Component/CameraMovementComponent/CameraMovementComponent.h"
+
+// Sets default values
+ACombatCameraPawn::ACombatCameraPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	mSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComponent"));
+
+	mCameraComponent = CreateDefaultSubobject<UCameraComponent>("Camera");
+	mCameraComponent->ProjectionMode = ECameraProjectionMode::Orthographic;
+	mCameraComponent->OrthoWidth = 1024.0f;
+	mCameraComponent->SetupAttachment(mSceneComponent);
+
+	mCameraMovementComponent = CreateDefaultSubobject<UCameraMovementComponent>("CameraMovement");
+	mCameraMovementComponent->SetCameraComponent(mCameraComponent);
+
+
+}
+
+// Called when the game starts or when spawned
+void ACombatCameraPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACombatCameraPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACombatCameraPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+UCameraComponent* ACombatCameraPawn::GetCameraComponent()
+{
+	return mCameraComponent.Get();
+}
+
+UCameraMovementComponent* ACombatCameraPawn::GetCameraMovementComponent()
+{
+	return mCameraMovementComponent.Get();
+}
+
