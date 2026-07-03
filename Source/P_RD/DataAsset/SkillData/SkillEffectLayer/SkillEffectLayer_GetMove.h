758f780a70091033d8dff40ccd24a7e38324cd74
Mo <98795422+codeGlutton@users.noreply.github.com>
2026-07-03 18:51:34 +0900

    feat Move Layer 추가
    [SVN-REV]: 137

---
+++ Source/P_RD/DataAsset/SkillData/SkillEffectLayer/SkillEffectLayer_GetMove.h
@@ -0,0 +1,33 @@
+﻿/*****************************************************************//**
+ * @file   SkillEffectLayer_GetMove.h
+ * @brief  하나의 스킬 모션 내에서 적용하는 움직임 포인트 습득 효과 단위 구현 헤더
+ * @author 모호재
+ * @date   2026-07-03
+ *********************************************************************/
+
+#pragma once
+
+#include "DataAsset/SkillData/SkillEffectLayer/SkillEffectLayer.h"
+#include "SkillEffectLayer_GetMove.generated.h"
+
+/**
+ * @brief  하나의 스킬 모션 내에서 적용하는 움직임 포인트 습득 효과 단위
+ */
+USTRUCT(BlueprintType)
+struct P_RD_API FSkillEffectLayer_GetMove : public FSkillEffectLayer
+{
+	GENERATED_BODY()
+
+public:
+	void ClearPointEffect(IBoardCombatTarget* ActorModel) const override;
+
+public:
+	void ApplyPointEffect(IBoardCombatTarget* ActorModel, float DiceSum) const override;
+	void CommitEffect(IBoardCombatTarget* ActorModel, const TArray<FTileIndex>& TargetTileIndexes, const TArray<IBoardCombatTarget*>& OtherCombatTargets, float DiceSum) const override;
+
+public:
+	UPROPERTY(Category = "Move", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "DefaultMoveGain"))
+	float mDefaultMoveGain = 0.f;
+	UPROPERTY(Category = "Move", EditAnywhere, BlueprintReadWrite, meta = (DisplayName = "DiceRatio"))
+	float mDiceRatio = 0.f;
+};
