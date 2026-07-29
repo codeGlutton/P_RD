ded9a509c46e6560ea8e1323cab2cc1a8cae9103
Kim JunHyeong <juny000903@gmail.com>
2026-07-29 21:11:00 +0900

    feat 직교 카메라 셰이크 제작 중...
    [SVN-REV]: 295

---
+++ Source/P_RD/Pawn/Camera/OrthographicCameraShakePattern.cpp
@@ -0,0 +1,85 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pawn/Camera/OrthographicCameraShakePattern.h"
+
+float FOrthographicNoiseShaker::Update(float DeltaTime, float AmplitudeMultiplier, float FrequencyMultiplier, float& InOutCurrentOffset) const
+{
+	const float TotalAmplitude = Amplitude * AmplitudeMultiplier;
+	if (TotalAmplitude != 0.f)
+	{
+		InOutCurrentOffset += DeltaTime * Frequency * FrequencyMultiplier;
+		return TotalAmplitude * FMath::PerlinNoise1D(InOutCurrentOffset);
+	}
+	return 0.f;
+}
+
+UOrthographicCameraShakePattern::UOrthographicCameraShakePattern(const FObjectInitializer& ObjInit)
+	: Super(ObjInit)
+{
+	// Default to only location shaking.
+	RotationAmplitudeMultiplier = 0.f;
+	OrthoWidth.Amplitude = 0.f;
+}
+
+void UOrthographicCameraShakePattern::StartShakePatternImpl(const FCameraShakePatternStartParams& Params)
+{
+	Super::StartShakePatternImpl(Params);
+
+	if (!Params.bIsRestarting)
+	{
+		// All offsets are random. This is because the core perlin noise implementation
+		// uses permutation tables, so if two shakers have the same initial offset and the same
+		// frequency, they will have the same exact values.
+		InitialLocationOffset = FVector3f((float)FMath::RandHelper(255), (float)FMath::RandHelper(255), (float)FMath::RandHelper(255));
+		InitialRotationOffset = FVector3f((float)FMath::RandHelper(255), (float)FMath::RandHelper(255), (float)FMath::RandHelper(255));
+		InitialOrthoWidthOffset = (float)FMath::RandHelper(255);
+
+		CurrentLocationOffset = InitialLocationOffset;
+		CurrentRotationOffset = InitialRotationOffset;
+		CurrentOrthoWidthOffset = InitialOrthoWidthOffset;
+	}
+}
+
+void UOrthographicCameraShakePattern::UpdateShakePatternImpl(const FCameraShakePatternUpdateParams& Params, FCameraShakePatternUpdateResult& OutResult)
+{
+	UpdateOrthographicNoise(Params.DeltaTime, OutResult);
+
+	const float BlendWeight = State.Update(Params.DeltaTime);
+	OutResult.ApplyScale(BlendWeight);
+}
+
+void UOrthographicCameraShakePattern::ScrubShakePatternImpl(const FCameraShakePatternScrubParams& Params, FCameraShakePatternUpdateResult& OutResult)
+{
+	// Scrubbing is like going back to our initial state and updating directly to the scrub time.
+	CurrentLocationOffset = InitialLocationOffset;
+	CurrentRotationOffset = InitialRotationOffset;
+	CurrentOrthoWidthOffset = InitialOrthoWidthOffset;
+
+	UpdateOrthographicNoise(Params.AbsoluteTime, OutResult);
+
+	const float BlendWeight = State.Scrub(Params.AbsoluteTime);
+	OutResult.ApplyScale(BlendWeight);
+}
+
+void UOrthographicCameraShakePattern::UpdateOrthographicNoise(float DeltaTime, FCameraShakePatternUpdateResult& OutResult)
+{
+	//OutResult.Location.X = X.Update(DeltaTime, LocationAmplitudeMultiplier, LocationFrequencyMultiplier, CurrentLocationOffset.X);
+	OutResult.Location.Y = Y.Update(DeltaTime, LocationAmplitudeMultiplier, LocationFrequencyMultiplier, CurrentLocationOffset.Y);
+	OutResult.Location.Z = Z.Update(DeltaTime, LocationAmplitudeMultiplier, LocationFrequencyMultiplier, CurrentLocationOffset.Z);
+	//UE_LOG(LogTemp, Log, TEXT("CurrentLocationOffset.X: %f"), CurrentLocationOffset.X);
+	//UE_LOG(LogTemp, Log, TEXT("CurrentLocationOffset.Y: %f"), CurrentLocationOffset.Y);
+
+
+	//OutResult.Rotation.Pitch = Pitch.Update(DeltaTime, RotationAmplitudeMultiplier, RotationFrequencyMultiplier, CurrentRotationOffset.X);
+	//OutResult.Rotation.Yaw = Yaw.Update(DeltaTime, RotationAmplitudeMultiplier, RotationFrequencyMultiplier, CurrentRotationOffset.Y);
+	OutResult.Rotation.Roll = Roll.Update(DeltaTime, RotationAmplitudeMultiplier, RotationFrequencyMultiplier, CurrentRotationOffset.Z);
+	UE_LOG(LogTemp, Log, TEXT("CurrentRotationOffset.Z: %f"), CurrentRotationOffset.Z);
+
+
+	//OutResult.FOV = FOV.Update(DeltaTime, 1.f, 1.f, CurrentFOVOffset);
+	OrthoWidth.Update(DeltaTime, 1.f, 1.f, CurrentOrthoWidthOffset);
+	//UE_LOG(LogTemp, Log, TEXT("CurrentOrthoWidthOffset: %f"), CurrentOrthoWidthOffset);
+}
+
+
