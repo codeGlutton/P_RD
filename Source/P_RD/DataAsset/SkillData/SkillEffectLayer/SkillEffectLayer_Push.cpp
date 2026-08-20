6ba41d073a9c06b66457083592e354c3006e3ca4
pyramidmine <pyramidmine@gmail.com>
2026-08-20 21:45:34 +0900

    feat 밀치기 스킬 이펙트 레이어 추가
    # 밀치기 스킬 레이어
      - SkillEffectLayer_Push 추가, 시전자가 바라보는 방향으로 대상을 밀침
      - 이동 중인 대상은 나머지 이동경로를 취소하고 밀치기 경로로 교체
      - 정지 중인 대상은 바로 밀기
      - 최대 밀치기 칸 수 지정 가능
    
    [SVN-REV]: 382

---
+++ Source/P_RD/DataAsset/SkillData/SkillEffectLayer/SkillEffectLayer_Push.cpp
@@ -0,0 +1,73 @@
+﻿/*****************************************************************//**
+ * @file   SkillEffectLayer_Push.cpp
+ * @brief  하나의 스킬 모션 내에서 적용하는 밀치기 효과 단위 구현 파일
+ * @author 이문환
+ * @date   2026-08-20
+ *********************************************************************/
+
+#include "DataAsset/SkillData/SkillEffectLayer/SkillEffectLayer_Push.h"
+
+#include "Singleton/WorldSubsystem/SRPGCombatModel.h"
+#include "Actor/TileMap/TileMapModel.h"
+#include "Actor/BoardActor/BoardActorModel.h"
+#include "Actor/BoardActor/BoardCombatTarget.h"
+#include "Component/BoardMovementComponent/BoardMovementComponentModel.h"
+
+void FSkillEffectLayer_Push::CommitEffect(const FSkillEffectCommitParams& Params) const
+{
+	// 시전자의 보드 정보 획득 (미는 방향 = 시전자가 바라보는 방향, 시전자 타일 = 연쇄 방지 키)
+	UBoardActorModel* InstigatorModel = Cast<UBoardActorModel>(Params.mInstigator.GetObject());
+	checkf(InstigatorModel != nullptr, TEXT("밀치기 시전자가 보드 액터가 아님"));
+
+	const FTileIndex InstigatorTileIndex = InstigatorModel->GetTileTransform().mIndex;
+	const ETileActorDirection PushDirection = InstigatorModel->GetTileTransform().mDirection;
+
+	USRPGCombatModel* CombatModel = GetWorldSubsystemModel<USRPGCombatModel>(InstigatorModel);
+	checkf(CombatModel != nullptr, TEXT("전투 모델 nullptr"));
+	UTileMapModel* TileMap = CombatModel->GetTileMap();
+	checkf(TileMap != nullptr, TEXT("타일 맵 nullptr"));
+
+	for (const TScriptInterface<IBoardCombatTarget>& Target : Params.mTargets)
+	{
+		UBoardActorModel* TargetModel = Cast<UBoardActorModel>(Target.GetObject());
+		UBoardMovementComponentModel* TargetMoveComp = Target->GetBoardMovementComponentModel();
+		if (TargetModel == nullptr || TargetMoveComp == nullptr)
+		{
+			continue;
+		}
+
+		// 밀리는 경로 계산 (뒤가 막히면 막히기 직전까지로 짧아짐)
+		const TArray<FTileIndex> PushPath = TileMap->GetPushPath(TargetModel->GetTileTransform().mIndex, PushDirection, mPushDistance);
+
+		// 한 칸도 밀리지 못하면 아무것도 안 함
+		if (PushPath.Num() < 2)
+		{
+			continue;
+		}
+
+		if (TargetMoveComp->IsMoving() == true)
+		{
+			// 이동 중인 대상: 등록만 하고, 이동 루프가 현재 스텝을 마무리하며 남은 경로를 밀치기 경로로 교체
+			TargetMoveComp->TryRegisterPendingPush(InstigatorTileIndex, PushPath);
+		}
+		else
+		{
+			// 정지 상태 대상: 즉시 밀기 시작 (방 시작 시 발판 위 배치 발동 등)
+			TargetMoveComp->PushAlongPath(PushPath);
+		}
+	}
+}
+
+#if WITH_EDITOR
+#define LOCTEXT_NAMESPACE "SkillEffectLayer_Push"
+
+FText FSkillEffectLayer_Push::MakeDescription() const
+{
+	return FText::Format(
+		LOCTEXT("PushFormat", "시전자가 바라보는 방향으로 {0}칸 밀어냅니다."),
+		FText::AsNumber(mPushDistance)
+	);
+}
+
+#undef LOCTEXT_NAMESPACE
+#endif
