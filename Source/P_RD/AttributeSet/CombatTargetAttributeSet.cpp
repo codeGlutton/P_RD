da848e1b857b616ab36b0f005c17ae92c23c508e
Mo <98795422+codeGlutton@users.noreply.github.com>
2026-09-09 20:03:17 +0900

    fix Attribute Pre/Post 처리 시에 Base 값에 대해서도 처리
    [SVN-REV]: 429

--- Source/P_RD/AttributeSet/CombatTargetAttributeSet.cpp
+++ Source/P_RD/AttributeSet/CombatTargetAttributeSet.cpp
@@ -1,4 +1,4 @@
-﻿#include "AttributeSet/CombatTargetAttributeSet.h"
+#include "AttributeSet/CombatTargetAttributeSet.h"
 #include "Component/AttributeComponent/AttributeSetComponentModel.h"
 #include "TAS/Effect/TacticalEffectContext.h"
 #include "TAS/Effect/Tag/TacticalEffect_Dead.h"
@@ -18,6 +18,17 @@ void UCombatTargetAttributeSet::PreAttributeChange(const FTacticalAttribute& Att
 	Super::PreAttributeChange(Attribute, NewValue);
 }
 
+void UCombatTargetAttributeSet::PreAttributeBaseChange(const FTacticalAttribute& Attribute, float& NewValue) const
+{
+	Super::PreAttributeBaseChange(Attribute, NewValue);
+
+	/* 체력 기본값 변경 시, 체력 초과 및 음수 방지 */
+	if (Attribute == GetHPAttribute())
+	{
+		NewValue = FMath::Clamp(NewValue, 0.f, GetMaxHP());
+	}
+}
+
 void UCombatTargetAttributeSet::PostAttributeChange(const FTacticalAttribute& Attribute, float OldValue, float NewValue)
 {
 	Super::PostAttributeChange(Attribute, OldValue, NewValue);
@@ -28,18 +39,53 @@ void UCombatTargetAttributeSet::PostAttributeChange(const FTacticalAttribute& At
 		if (GetHP() > NewValue)
 		{
 			UAttributeSetComponentModel* ASC = GetOwningAttributeSetComponentModel();
-			ASC->ApplyModToAttribute(GetHPAttribute(), ETacticalModOp::Override, NewValue);
+			if (ASC != nullptr)
+			{
+				ASC->ApplyModToAttribute(GetHPAttribute(), ETacticalModOp::Override, NewValue);
+			}
 		}
 	}
 
 	if (Attribute == GetHPAttribute() && OldValue > 0.f && NewValue <= 0.f)
 	{
 		UAttributeSetComponentModel* ASC = GetOwningAttributeSetComponentModel();
+		if (ASC != nullptr)
+		{
+			UTacticalEffectContext* EffectContext = ASC->MakeEffectContext();
 
-		UTacticalEffectContext* EffectContext = ASC->MakeEffectContext();
+			TSharedPtr<FTacticalEffectSpec> InfiniteEffect = ASC->MakeOutgoingSpec(UTacticalEffect_GetDead::StaticClass(), EffectContext);
+			FActiveTacticalEffectHandle ActiveHandle = ASC->ApplyTacticalEffectSpecToSelf(*InfiniteEffect);
+		}
+	}
+}
 
-		TSharedPtr<FTacticalEffectSpec> InfiniteEffect = ASC->MakeOutgoingSpec(UTacticalEffect_GetDead::StaticClass(), EffectContext);
-		FActiveTacticalEffectHandle ActiveHandle = ASC->ApplyTacticalEffectSpecToSelf(*InfiniteEffect);
+void UCombatTargetAttributeSet::PostAttributeBaseChange(const FTacticalAttribute& Attribute, float OldValue, float NewValue) const
+{
+	Super::PostAttributeBaseChange(Attribute, OldValue, NewValue);
+
+	/* 체력 최댓값 기본값 변경 시, 체력 초과 방지 */
+	if (Attribute == GetMaxHPAttribute())
+	{
+		if (GetHP() > NewValue)
+		{
+			UAttributeSetComponentModel* ASC = GetOwningAttributeSetComponentModel();
+			if (ASC != nullptr)
+			{
+				ASC->ApplyModToAttribute(GetHPAttribute(), ETacticalModOp::Override, NewValue);
+			}
+		}
+	}
+
+	if (Attribute == GetHPAttribute() && OldValue > 0.f && NewValue <= 0.f)
+	{
+		UAttributeSetComponentModel* ASC = GetOwningAttributeSetComponentModel();
+		if (ASC != nullptr)
+		{
+			UTacticalEffectContext* EffectContext = ASC->MakeEffectContext();
+
+			TSharedPtr<FTacticalEffectSpec> InfiniteEffect = ASC->MakeOutgoingSpec(UTacticalEffect_GetDead::StaticClass(), EffectContext);
+			FActiveTacticalEffectHandle ActiveHandle = ASC->ApplyTacticalEffectSpecToSelf(*InfiniteEffect);
+		}
 	}
 }
 
--- Source/P_RD/AttributeSet/UnitAttributeSet.cpp
+++ Source/P_RD/AttributeSet/UnitAttributeSet.cpp
@@ -1,4 +1,4 @@
-﻿#include "AttributeSet/UnitAttributeSet.h"
+#include "AttributeSet/UnitAttributeSet.h"
 #include "Component/AttributeComponent/AttributeSetComponentModel.h"
 #include "TAS/Effect/TacticalEffectContext.h"
 
@@ -15,9 +15,25 @@ void UUnitAttributeSet::PreAttributeChange(const FTacticalAttribute& Attribute, 
 	Super::PreAttributeChange(Attribute, NewValue);
 }
 
+void UUnitAttributeSet::PreAttributeBaseChange(const FTacticalAttribute& Attribute, float& NewValue) const
+{
+	/* 충전 값 감소 시, 마이너스 방지 */
+	if (Attribute == GetRechargeActionPointAttribute() || Attribute == GetRechargeSpeedPointAttribute())
+	{
+		NewValue = FMath::Max(NewValue, 0.f);
+	}
+
+	Super::PreAttributeBaseChange(Attribute, NewValue);
+}
+
 void UUnitAttributeSet::PostAttributeChange(const FTacticalAttribute& Attribute, float OldValue, float NewValue)
 {
 	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+}
+
+void UUnitAttributeSet::PostAttributeBaseChange(const FTacticalAttribute& Attribute, float OldValue, float NewValue) const
+{
+	Super::PostAttributeBaseChange(Attribute, OldValue, NewValue);
 }
 
 void UPlayerUnitAttributeSet::PreAttributeChange(const FTacticalAttribute& Attribute, float& NewValue)
@@ -31,9 +47,43 @@ void UPlayerUnitAttributeSet::PreAttributeChange(const FTacticalAttribute& Attri
 	Super::PreAttributeChange(Attribute, NewValue);
 }
 
+void UPlayerUnitAttributeSet::PreAttributeBaseChange(const FTacticalAttribute& Attribute, float& NewValue) const
+{
+	/* 경험치 감소 시, 마이너스 방지 */
+	if (Attribute == GetExpAttribute())
+	{
+		NewValue = FMath::Max(NewValue, 0.f);
+	}
+
+	Super::PreAttributeBaseChange(Attribute, NewValue);
+}
+
 void UPlayerUnitAttributeSet::PostAttributeChange(const FTacticalAttribute& Attribute, float OldValue, float NewValue)
 {
 	Super::PostAttributeChange(Attribute, OldValue, NewValue);
+
+	/* 레벨업 처리 */
+	if (Attribute == GetExpAttribute())
+	{
+		UAttributeSetComponentModel* ASC = GetOwningAttributeSetComponentModel();
+		if (ASC == nullptr)
+		{
+			return;
+		}
+
+		UPlayerUnitModel* PlayerModel = ASC->GetOwnerModel<UPlayerUnitModel>();
+		if (PlayerModel == nullptr)
+		{
+			return;
+		}
+
+		PlayerModel->PostChangeExperience(OldValue, NewValue);
+	}
+}
+
+void UPlayerUnitAttributeSet::PostAttributeBaseChange(const FTacticalAttribute& Attribute, float OldValue, float NewValue) const
+{
+	Super::PostAttributeBaseChange(Attribute, OldValue, NewValue);
 
 	/* 레벨업 처리 */
 	if (Attribute == GetExpAttribute())
